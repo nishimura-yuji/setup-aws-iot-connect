f8602d9cc5a4bf5c9bb02be5610c14f896fb3b34
nishimura-yuji <nishimura.yuji@classmethod.jp>
2018-09-23 17:35:42 +0900

    first commit

---
+++ setup-connect-aws-iot.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+#----------------------------------------------------------
+# 引数があるか確認
+#----------------------------------------------------------
+if [ "$1" = "" ]
+then
+    echo -e "Error:no argument.  \nex) setup-connect-aws-iot.sh <thing name>"
+    exit
+fi
+
+#----------------------------------------------------------
+# 変数設定
+#----------------------------------------------------------
+cd `dirname $0`
+
+readonly BASE_DIR='.'
+
+readonly CERTIFICATE_PEM=${BASE_DIR}/cert-${1}.crt
+readonly PRIVATE_KEY=${BASE_DIR}/private-${1}.key
+readonly PUBLIC_KEY=${BASE_DIR}/public-${1}.key
+
+readonly ROOT_CA_PEM=${BASE_DIR}/rootCA.pem
+readonly ROOT_CA_DL_LINK='https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem'
+
+
+#----------------------------------------------------------
+#  root CA 証明書を確認:ないとき取得
+#----------------------------------------------------------
+if [ ! -e ${ROOT_CA_PEM} ]; then
+    # echo -n "Error: CAroot.pem or CAroot.key or rrootCAoot.pem not exists."
+    wget ${ROOT_CA_DL_LINK} -O ${ROOT_CA_PEM} > /dev/null 2>&1
+fi
+
+
+#----------------------------------------------------------
+#  AWS IoTの設定:証明書と鍵がないとき作成
+#----------------------------------------------------------
+if [ ! -e ${CERTIFICATE_PEM} ] || [ ! -e ${PRIVATE_KEY} ] || [ ! -e ${PUBLIC_KEY} ]; then
+    # echo -n "Pass: Certificate Files Already Exists."
+
+    # {thing name}-policy としてのIAM Policyを作成
+    aws iot create-policy --policy-name ${1}-policy --policy-document '{"Version": "2012-10-17","Statement": [{"Effect": "Allow","Action": "iot:*","Resource": "*"}]}'
+
+    # thing 作成
+    aws iot create-thing --thing-name ${1}
+
+    # 証明書と鍵を作成
+    ARN_OF_CERTIFICATE=`aws iot create-keys-and-certificate --set-as-active --certificate-pem-outfile ${CERTIFICATE_PEM} --private-key-outfile ${PRIVATE_KEY} --public-key-outfile ${PUBLIC_KEY} --query "certificateArn"`
+    echo ${ARN_OF_CERTIFICATE}
+
+    # Policyと証明書を紐付け
+    eval aws iot attach-principal-policy --policy-name ${1}-policy --principal ${ARN_OF_CERTIFICATE}
+
+    # Thingと証明書を紐付け
+    eval aws iot attach-thing-principal  --thing-name ${1} --principal ${ARN_OF_CERTIFICATE}
+fi
+
+
+#----------------------------------------------------------
+#  AWS IoTのエンドポイント取得
+#----------------------------------------------------------
+ENDPOINT=`aws iot describe-endpoint --query "endpointAddress"`
+
+
+#----------------------------------------------------------
+#  トピック「topic/test」にPublish
+#----------------------------------------------------------
+CMD="mosquitto_pub --cafile ${ROOT_CA_PEM} --cert ${CERTIFICATE_PEM} --key ${PRIVATE_KEY} -h ${ENDPOINT} -p 8883 -q 1 -d -t topic/test -i ${1}-id -m '{\"key\": \"hello world from ThingName:${1}\"}'"
+eval ${CMD}
